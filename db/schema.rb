87e857b4d4297e3f1a7fe09b107062dbb689d4b2
Norma Lopez <norma_l304@yahoo.com>
2020-07-17 09:33:05 -0600

    Add RideMechanics table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200717152943) do
+ActiveRecord::Schema.define(version: 20200717153220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20200717152943) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "ride_mechanics", force: :cascade do |t|
+    t.bigint "ride_id"
+    t.bigint "mechanic_id"
+    t.index ["mechanic_id"], name: "index_ride_mechanics_on_mechanic_id"
+    t.index ["ride_id"], name: "index_ride_mechanics_on_ride_id"
+  end
+
   create_table "rides", force: :cascade do |t|
     t.string "name"
     t.float "thrill_rating"
@@ -36,4 +43,6 @@ ActiveRecord::Schema.define(version: 20200717152943) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "ride_mechanics", "mechanics"
+  add_foreign_key "ride_mechanics", "rides"
 end
