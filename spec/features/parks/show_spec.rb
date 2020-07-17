cb8a46533b397fb5e8338a15977394736f9cd939
Norma Lopez <norma_l304@yahoo.com>
2020-07-17 11:03:50 -0600

    Add test to display all rides in alphabetical order

--- spec/features/parks/show_spec.rb
+++ spec/features/parks/show_spec.rb
@@ -8,6 +8,19 @@ RSpec.describe 'Amusement park show page' do
 
     expect(page).to have_content("Disneyland")
     expect(page).to have_content("Admissions: $120")
+  end
 
+  it 'displays names of all rides in alphabetical order' do
+    disney = Park.create!(name: "Disneyland", admission_price: "$120")
+    splash_mountain = disney.rides.create!(name: "Splash Mountain", thrill_rating: 7.8)
+    haunted_mansion = disney.rides.create!(name: "The Haunted Mansion", thrill_rating: 8.2)
+    autopia = disney.rides.create!(name: "Autopia", thrill_rating: 3.5)
+
+    visit "/parks/#{disney.id}"
+
+    expect(page).to have_content("Rides:")
+    expect(page).to have_content("Autopia")
+    expect(page).to have_content("Splash Mountain")
+    expect(page).to have_content("The Haunted Mansion")
   end
 end
