d47f9139655febabecb7041e15b5bbe266758d14
Norma Lopez <norma_l304@yahoo.com>
2020-07-17 11:03:08 -0600

    Add test for #alphabetical_rides

--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe Park do
   describe 'relationships' do
     it {should have_many :rides}
   end
+
+  describe 'methods' do
+    it '#alphabetical_rides' do
+      disney = Park.create!(name: "Disneyland", admission_price: "$120")
+      splash_mountain = disney.rides.create!(name: "Splash Mountain", thrill_rating: 7.8)
+      haunted_mansion = disney.rides.create!(name: "The Haunted Mansion", thrill_rating: 8.2)
+      autopia = disney.rides.create!(name: "Autopia", thrill_rating: 3.5)
+
+      expect(disney.alphabetical_rides).to eq([autopia, splash_mountain, haunted_mansion])
+    end
+  end
 end
